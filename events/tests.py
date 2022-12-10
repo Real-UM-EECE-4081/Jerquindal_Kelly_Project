c0d40abc88ea3dd189003a03bbbc94d888f149da
JJ Kelly <88286005+JJKelly0@users.noreply.github.com>
2022-12-10 08:59:09 -0600

    Update tests.py

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from events.models import TrainingSite
 
-# Create your tests here.
+
+class TrainingTestCase(TestCase):
+    def setUp(self):
+        TrainingSite.objects.create(name='HaydenHall', address='3795 Desoto', zip_code='38152', phone='123-45-6789',
+                                    owner='1')
+
+
+class EventTestCase(TestCase):
+    def setUp(self):
+        TrainingSite.objects.create(name='HaydenHall', address='3795 Desoto', zip_code='38152', phone='123-45-6789',
+                                    owner='1')
