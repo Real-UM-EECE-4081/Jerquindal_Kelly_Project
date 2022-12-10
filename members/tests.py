7e8b19e97bcbf441cd6ee7391fa031e82daba03f
JJ Kelly <88286005+JJKelly0@users.noreply.github.com>
2022-12-10 08:58:43 -0600

    Update tests.py

--- members/tests.py
+++ members/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model, get_user
 from django.urls import reverse
-from signup_app.models import Contacts
 
 # Create your tests here.
 class Tests(TestCase):
@@ -31,17 +30,4 @@ class Tests(TestCase):
             self.assertEqual(response.status_code, 302)
             users = get_user_model().objects.all()
             self.assertEqual(users.count(), 1)
-    
-     
-    def test_login(self):
-        response = self.client.get('/signin')
-        self.assertEquals(response.status_code, 200)
-        self.client.login(username= self.username, password=self.email)
-        self.assertTrue('Log in' in response.content)
-
-    def test_logout(self):
-        self.client.login(username= self.username, password=self.email)
-        response = self.client.get('/signin')
-        self.assertEquals(response.status_code, 200)
-        self.assertTrue('Log out' in response.content)
-        self.client.logout()
+   
